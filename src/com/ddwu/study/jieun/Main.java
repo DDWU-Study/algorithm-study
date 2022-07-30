c1075f6eeaba3acb9a5c12690732bd08ce4b49b6
leejieuns2 <2jjingni@gmail.com>
2022-07-31 00:07:54 +0900

    add example java files

---
+++ src/com/ddwu/study/jieun/Main.java
@@ -0,0 +1,7 @@
+package com.ddwu.study.jieun;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("jieun");
+    }
+}
---
+++ src/com/ddwu/study/taeyeon/Main.java
@@ -0,0 +1,7 @@
+package com.ddwu.study.taeyeon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("taeyeon");
+    }
+}
