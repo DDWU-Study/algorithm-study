a0f53739696d05c8f3972e0ea8be55feca1fbab9
hongbi-lee <foxgirin950@gmail.com>
2022-08-19 23:35:16 +0900

    Feat: solve yinyang and dot product

---
+++ src/com/ddwu/study/soyeon/0820/lv1_dot_product.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += a[i] * b[i]
+    return answer       #  return sum([x*y for x, y in zip(a,b)])
+
+
+if __name__ == '__main__':
+    a = [1, 2, 3, 4]
+    b = [-3, -1, 0, 2]
+    print(solution(a, b))
