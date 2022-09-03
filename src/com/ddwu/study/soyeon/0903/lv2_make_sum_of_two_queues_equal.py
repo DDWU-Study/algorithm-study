4d9d236df47cf419f4abe9a35cd28d753c5ffaec
hongbi-lee <foxgirin950@gmail.com>
2022-09-03 21:20:33 +0900

    edit 두 큐 합 같게 만들기

--- src/com/ddwu/study/soyeon/0903/lv2_make_sum_of_two_queues_equal.py
+++ src/com/ddwu/study/soyeon/0903/lv2_make_sum_of_two_queues_equal.py
@@ -1,10 +1,21 @@
-def solution(queue1, queue2):
-    answer = -2
-    total = int((sum(queue1) + sum(queue2)) / 2)
+from collections import deque
 
-    print(queue1.pop(0))
-    queue1.insert(len(queue1), 0)
-    print(queue1)
+
+def solution(queue1, queue2):
+    queue1, queue2 = deque(queue1), deque(queue2)
+    total = int((sum(queue1) + sum(queue2)) / 2)    # 원래 내가 하고 싶었던 방법
+    queue1_sum = sum(queue1)
+    answer = 0
+    while queue1 and queue2:
+        if queue1_sum == total:
+            return answer
+        elif queue1_sum > total:
+            queue1_sum -= queue1[0]
+            queue2.append(queue1.popleft())
+        else:
+            queue1_sum += queue2[0]
+            queue1.append(queue2.popleft())
+        answer += 1
     return answer
 
 
