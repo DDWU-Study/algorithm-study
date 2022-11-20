39050bb868b7993a487cfafa6277c292cc624e41
eun <eunmoong@gmail.com>
2022-11-20 21:26:11 +0900

    1120

--- src/com/ddwu/study/eunyoung/programmers/_221120/피보나치수.java
+++ src/com/ddwu/study/eunyoung/programmers/_221120/피보나치수.java
@@ -1,4 +1,16 @@
 package src.com.ddwu.study.eunyoung.programmers._221120;
 
 public class 피보나치수 {
+    public static int solution(int n) {
+          //  int answer = 0;
+            return fibo(n);
+        }
+    public static  int fibo(int n){
+        if(n==0 || n==1) return n;
+
+        return fibo(n-1)+fibo((n-2));
+    }
+    public static void main (String[] args){
+        System.out.println(solution(5));
+    }
 }
