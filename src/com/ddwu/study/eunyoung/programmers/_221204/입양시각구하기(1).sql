f24069d7a536ee1c091bc28bee52581d8ab7cf8a
eun <eunmoong@gmail.com>
2022-12-04 13:59:35 +0900

    1204

--- src/com/ddwu/study/eunyoung/programmers/_221204/입양시각구하기(1).sql
+++ src/com/ddwu/study/eunyoung/programmers/_221204/입양시각구하기(1).sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
 SELECT hour(datetime) HOUR,count(ANIMAL_ID) COUNT
 from animal_outs
-where hour(datetime) >= 9
+where hour(datetime) >= 9 and hour (datetime)<=19
 group by hour(datetime)
 -- 이거 안됨having hour(datetime) >= 9
 -- having hour >= 9
